fe4e32941e2e20768f359a4eeb7f46778cd55718
Caio Tomaz <caiotomaza@gmail.com>
2025-04-24 21:06:58 -0300

    feat - criação do db junto com a função de conexão

--- src/database/connection.php
+++ src/database/connection.php
@@ -1,29 +1,26 @@
 <?php
 
-    namespace Src\Database;
-
     class Conexao {
         private static $instance;
 
-        public static function getConn(){
+        public static function getConn() {
             try {
-                if (!isset(self::$instance)){
-                    self::$instance = new \PDO('pgsql:host=localhost;dbname=e_commercer', 'postgres', 'root');
+                if (!isset(self::$instance)) {
+                    self::$instance = new \PDO(
+                        'mysql:host=localhost;dbname=fluent_ia_db;charset=utf8', // Mudança aqui
+                        'root',      // Usuário padrão do XAMPP
+                        ''           // Senha padrão do XAMPP (geralmente vazia)
+                    );
 
                     self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 }
+
                 return self::$instance;
             } catch (\PDOException $e) {
                 echo "Erro de conexão: " . $e->getMessage();
-
-                return null; 
+                return null;
             }
         }
     }
-
-    /*
-        Quando for configurar no Xampp (em modo administrador) o banco use apenas o Apache e altera uma configuração dele, no PHP(php.ini), remova o ";" de extension=pdo_pgsql e salve depois reinicio tudo.
-        Observação lembre de manter o usuario "postgres" e senha "root"!!!
-    */
 
 ?>
